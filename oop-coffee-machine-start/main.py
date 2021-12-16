5138601d411a46e0d39a2653e56f0e47beddca58
zzzturka <zeturka@gmail.com>
2021-12-16 21:50:15 +1300

    test

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -4,32 +4,25 @@ from money_machine import MoneyMachine
 state = True
 profit = 0
 coffee = Menu()
-
 money = MoneyMachine()
-
-
 maintenance = CoffeeMaker()
 
-
-
-
+# print(coffee.menu[0].ingredients["milk"])
+# print(getattr(Menu(), milk)
 while state:
     choice = input(f"What would you like? {coffee.get_items()}:  ").lower()
-    # coffee.find_drink(choice)
-    print(coffee.menu[2]["cost"])
-    # if choice in coffee.get_items():
-    #     if maintenance.is_resource_sufficient(coffee.find_drink(choice)):
-    #         money.make_payment(coffee.find_drink(choice))
-    #         maintenance.make_coffee(coffee.find_drink(choice))
-    #     else:
-    #         break
-    #
-    # elif choice == "off":
-    #     state = False
-    # elif choice == "report":
-    #     maintenance.report()
-    #     money.report()
-
+    if choice in coffee.get_items():
+        if maintenance.is_resource_sufficient(coffee.find_drink(choice)):
+            if money.make_payment(coffee.find_drink(choice).cost):
+                maintenance.make_coffee(coffee.find_drink(choice))
+        else:
+            break
+    elif choice == "off":
+        state = False
+    elif choice == "report":
+        maintenance.report()
+        money.report()
+print("thanks for using our coffee machine!")
 
 
 
